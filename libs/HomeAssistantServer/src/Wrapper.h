cb2ac945d30a0fc6c923cb78ce9fa34588d63add
Static <210101922+Static-Codes@users.noreply.github.com>
2026-01-07 14:50:55 -0500

    Added cross platform compliant code for wrapper

--- libs/HomeAssistantServer/src/Wrapper.h
+++ libs/HomeAssistantServer/src/Wrapper.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "./includeall.hpp"
+#include <string>
+#include <vector>
 
 // Stopping Clangd from screaming at me for not being on Windows - Static
 #if defined(_WIN32)
@@ -7,6 +9,111 @@
 #else
     #define EXPORT __attribute__((visibility("default")))
 #endif
+
+enum class ManagedServerStatus
+{
+    Inactive   = 1,
+    Running   = 2,
+    Restarting = 3,
+    Terminated = 4
+};
+
+enum class ManagedFeatureName
+{
+    WeatherForecast = 0,
+    WebSearch       = 1,
+    StockPrices     = 2
+};
+
+enum class ManagedFeatureStatus
+{
+    Available              = 0,
+    CurrentlyBroken        = 1,
+    Finished               = 2,
+    InProgress             = 3,
+    TemporarilyUnavailable = 4
+};
+
+//
+// Managed Feature wrapper
+//
+
+class ManagedFeature
+{
+    private: 
+        Feature* native;
+        
+    public:
+        ManagedFeature(Feature* feature) : native(feature) { }
+
+    ManagedFeatureName GetName() {
+        return static_cast<ManagedFeatureName>(native->name);
+    };
+
+    ManagedFeatureStatus GetStatus()
+    {
+        return static_cast<ManagedFeatureStatus>(native->getStatus());
+    };
+
+    void set(ManagedFeatureStatus value)
+    {
+        native->setStatus(static_cast<FeatureStatus>(value));
+    };
+};
+
+//
+// Managed HomeAssistantServer wrapper
+//
+
+class ManagedHomeAssistantServer
+{
+    private:
+        HomeAssistantServer* native;
+
+    public:
+        ManagedHomeAssistantServer() {
+            native = new HomeAssistantServer();
+        }
+        
+        // Standard destructor as opposed to the Windows specific one, previous here - Static
+        ~ManagedHomeAssistantServer() = default;
+        
+        
+        // !ManagedHomeAssistantServer()
+        // {
+        //     delete native;
+        //     native = nullptr;
+        // }
+
+        // Using a reference to the pointer to avoid syntax issues - Static
+        ManagedHomeAssistantServer(const ManagedHomeAssistantServer&) = delete;
+
+        ManagedServerStatus GetStatus()
+        {
+            return static_cast<ManagedServerStatus>(native->getStatus());
+        }
+
+        void SetStatus(ManagedServerStatus status)
+        {
+            native->setStatus(static_cast<ServerStatus>(status));
+        }
+        
+        // Using the native c++ string vector as opposed to C# style string array
+        void Start(std::vector<std::string> args)
+        {
+            native->start(args.data());
+        }
+
+        bool Restart()
+        {
+            return native->restart();
+        }
+
+        void Terminate()
+        {
+            native->terminate();
+        }
+};
 
 // This is annoying but this is the cost of cross platform development - Static
 extern "C" {
